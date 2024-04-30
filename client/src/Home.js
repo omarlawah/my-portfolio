e70ea75dd2619e1b09ae6d02ab4f3a5f6ce739b0
omarlawah <mdawg5373@gmail.com>
2024-04-30 17:55:06 +0000

    modified home page

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,41 +1,46 @@
 import {Container, Row, Col} from 'react-bootstrap';
 import { useState, useEffect } from 'react';
-const Home = () => {
-    const phrases = ["Welcome to my portfolio!", "I'm a web developer", "I'm a web designer"];
-    const [phrase, setPhrase] = useState("");
-    let [i, setI] = useState(0);
-    useEffect(() => {
-        let intervalId = setInterval(() => {
-            setPhrase(phrases[i]);
-            console.log(i);
-            setI(i++); 
-            if (i === 3) {
-                clearInterval(intervalId);
-            }
-        }, 1000);
-    
-        return () => {
-            clearInterval(intervalId);
-        };
-    });
 
-    
+
+
+const Home = () => {
    
-   
+    const phrases = ["Welcome to my portfolio!", "I'm a web designer", "I'm a web developer"];
+    const [phrase, setPhrase] = useState();
+    // function to make the intro text change every 0.1 second
+    // useEffect(() => {
+    //     let phraseIndex = 0;
+    //     let letterIndex = 0;
+    //     const intervalId = setInterval(() => {
+    //         if (letterIndex <= phrases[phraseIndex].length) {
+    //             setPhrase(phrases[phraseIndex].slice(0, letterIndex));
+    //                 letterIndex++;
+    //         } else {
+    //             letterIndex = 0;
+    //             phraseIndex = (phraseIndex + 1) % phrases.length;
+    //         }
+    //     }, 100); 
+
+    //     return () => clearInterval(intervalId);
+    // }, []);
 
     return (
+        <>
         <div  className="home-container"> 
             <Container fluid className="home-content"> 
                 <Row id='intro'>
                     <Col>
                         <p><span>{phrase}</span><br/>As a passionate web developer, I specialize 
                          in crafting engaging and functional web experiences. With a keen eye for design and a commitment
-                         to clean code, I strive to bring ideas to life through innovative solutions. Explore my projects
-                          below to see how I can help elevate your online presence.</p>
+                          below to see how I can help elevate your online presence with clean code, I strive to bring ideas
+                           to life through innovative solutions.</p>
+                         
                     </Col>
                 </Row>
             </Container>
         </div>
+       
+        </>
      );
 }
  
--- client/src/Services.js
+++ client/src/Services.js
@@ -1,7 +1,7 @@
 const Services = () => {
     return ( 
         <>
-            <h1>This is services</h1>
+        <h1>serviecessm</h1>
         </>
      );
 }
