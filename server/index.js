d0f374519bae12bb4a7d998c4cf50aa9be4dc92c
omarlawah <mdawg5373@gmail.com>
2024-05-21 00:16:46 +0000

    adding autho-generated email

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 app.use(cors());
+const nodemailer = require('nodemailer');
 const dotenv = require('dotenv');
 dotenv.config({path: './.env'});  
 const bodyParser = require('body-parser');
@@ -17,12 +18,39 @@ DB_Connection();
 
 // middleware to handle form submission route 
 app.post('/Form/submit', async (req,res,next) =>{
+    let userMail = req.body.email;
    try{
         const customer = new Customer(req.body);
         await customer.save()
         .then(()=>{
             console.log("new customer added: ", customer);
-        })
+ 
+            let transporter = nodemailer.createTransport({
+                service: 'gmail',
+                auth: {
+                  user: process.env.EMAIL,
+                  pass: process.env.PASS
+                },
+                 tls: {
+    // Do not fail on invalid certs
+    rejectUnauthorized: false
+  }
+              });
+              
+              let mailOptions = {
+                from: process.env.EMAIL,
+                to: userMail,
+                subject: 'Test Email',
+                text: 'Hello, this is a test email sent using Nodemailer!'
+              };
+              
+              transporter.sendMail(mailOptions, (error, info) => {
+                if (error) {
+                  return console.log(error);
+                }
+                console.log('Email sent: ' + info.response);
+              });
+            })
         res.status(200).json({
             message: "success",
             data: req.body
